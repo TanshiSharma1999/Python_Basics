602603d2122164d05982252f9bbad72ee0ce8b78
Tanshi Sharma <120782697+TanshiSharma1999@users.noreply.github.com>
2026-08-12 00:16:03 +0530

    Add files via upload

---
+++ Dictionary/prob4.py
@@ -0,0 +1,12 @@
+#clear a dictionary
+fruits= {"Mango": 100, "kiwi": 25, "oranges": 18}
+
+print(fruits)
+fruits.clear()
+print(fruits)
+# merge 2 dictionaries
+dict1 = {"a": 1, "b": 2} 
+dict2 = {"b": 3, "c": 4}
+
+dict1.update(dict2)
+print(dict1)
